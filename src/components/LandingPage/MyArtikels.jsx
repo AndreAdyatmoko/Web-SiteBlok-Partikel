75c64458a038886f41fd2d177b02df81f0559d6f
AndreAdyatmoko <AndreAdyatmoko>
2023-06-28 16:07:23 +0700

    My Articles rapi

--- src/components/LandingPage/MyArtikels.jsx
+++ src/components/LandingPage/MyArtikels.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Heading, Text, VStack, Image, HStack } from '@chakra-ui/react';
+import { Box, Heading, Text, Image, HStack, Wrap, WrapItem } from '@chakra-ui/react';
 import axios from 'axios';
 
 const MyArtikels = () => {
@@ -30,34 +30,35 @@ const MyArtikels = () => {
     <Box mt={90} align="center">
       <Heading mb={5}>My Artikels</Heading>
       {articles.length > 0 ? (
-        <VStack spacing={5}>
+        <Wrap spacing={5} justify="center">
           {articles.map((article) => (
-            <Box
-              key={article.id}
-              p={5}
-              borderWidth="1px"
-              borderRadius="md"
-              width="500px"
-              boxShadow="md"
-            >
-              <Heading size="md">{article.title}</Heading>
-              <Image
-                src={`https://minpro-blog.purwadhikabootcamp.com/${article.imageURL}`}
-                alt={article.title}
-                mt={4}
-                mb={4}
-                objectFit="cover"
-                height="200px"
-                width="100%"
-              />
-              <Text>{article.content}</Text>
-              <HStack spacing={2} mt={4}>
-                <Text fontWeight="bold">Category:</Text>
-                <Text>{article.category}</Text>
-              </HStack>
-            </Box>
+            <WrapItem key={article.id}>
+              <Box
+                p={5}
+                borderWidth="1px"
+                borderRadius="md"
+                width="500px"
+                boxShadow="md"
+              >
+                <Heading size="md">{article.title}</Heading>
+                <Image
+                  src={`https://minpro-blog.purwadhikabootcamp.com/${article.imageURL}`}
+                  alt={article.title}
+                  mt={4}
+                  mb={4}
+                  objectFit="cover"
+                  height="200px"
+                  width="100%"
+                />
+                <Text>{article.content}</Text>
+                <HStack spacing={2} mt={4}>
+                  <Text fontWeight="bold">Category:</Text>
+                  <Text>{article.Category.name}</Text>
+                </HStack>
+              </Box>
+            </WrapItem>
           ))}
-        </VStack>
+        </Wrap>
       ) : (
         <Text>No articles found.</Text>
       )}
